2e9b704ebfefbe1efc41eeba3faff0ad9e098a78
99minj0731 <99minj0731@naver.com>
2024-11-25 22:49:57 +0900

    11.25.min.23pm

--- django-pjt/accounts/serializers.py
+++ django-pjt/accounts/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id','username', 'profile_img', 'like_movies')
+        fields = '__all__'
 
 # 프로필 이미지
 
